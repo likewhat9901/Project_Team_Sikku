e25c130569a5bac09a727ea446355b55c510c1ef
likewhat9901 <parkko123@naver.com>
2025-08-08 13:03:13 +0900

    test폴더 변경

--- src/test/java/com/edu/sikku/SikkuApplicationTests.java
+++ src/test/java/com/edu/sikku/SikkuApplicationTests.java
@@ -1,10 +1,10 @@
-package com.edu.springboot;
+package com.edu.sikku;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class B07aMybatisApplicationTests {
+class SikkuApplicationTests {
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/edu/springboot/SikkuApplicationTests.java
+++ src/test/java/com/edu/springboot/SikkuApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.edu.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SikkuApplicationTests {
 
 	@Test
 	void contextLoads() {
