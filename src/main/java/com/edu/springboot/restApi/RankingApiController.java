4baadf5d8c26533e12832431bfa2f5e036dc85b7
likewhat9901 <parkko123@naver.com>
2025-08-11 11:30:01 +0900

    Resolve: final merge with jinkyeong

--- src/main/java/com/edu/springboot/restApi/RankingApiController.java
+++ src/main/java/com/edu/springboot/restApi/RankingApiController.java
@@ -4,7 +4,6 @@ package com.edu.springboot.restApi;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
